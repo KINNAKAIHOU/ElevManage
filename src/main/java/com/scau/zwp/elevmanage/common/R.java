0b295b285ad05413a01b4468c8d5d1f5ebc44ee2
ihou <2570719876@qq.com>
2023-02-19 17:50:35 +0800

    电梯进一步完善

--- src/main/java/com/scau/zwp/elevmanage/common/R.java
+++ src/main/java/com/scau/zwp/elevmanage/common/R.java
@@ -13,7 +13,7 @@ import java.util.Objects;
 @Data
 public class R<T> {
 
-    private Integer code; //编码：1成功，0和其它数字为失败
+    private Integer code; //编码：1成功，和其它数字为失败
 
     private String msg; //错误信息
 
@@ -22,14 +22,14 @@ public class R<T> {
     public static <T> R<T> success(T object) {
         R<T> r = new R<T>();
         r.data = object;
-        r.code = 1001;
+        r.code = 1;
         return r;
     }
 
     public static <T> R<T> error(String msg) {
         R r = new R();
         r.msg = msg;
-        r.code = 1000;
+        r.code = 0;
         return r;
     }
 
